dd0e330b63b433f640fea22aacee55f118c1da86
Alicja Cichocka <alka.975@gmail.com>
2024-08-26 20:20:38 +0200

    Zadanie zmienne

---
+++ kodilla-intro/src/main/java/com/kodilla/MainExtended.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Alicja";
+        String surname = "Cichocka";
+        int age = 27;
+        double height = 1.68;
+        char firstLetterOfSurname = 'C';
+        boolean isKodillaStudent = true;
+    }
+}
