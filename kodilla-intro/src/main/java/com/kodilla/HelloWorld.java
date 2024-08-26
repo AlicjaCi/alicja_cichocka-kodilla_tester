26e794ad2c4065f67bf0a3e188b20ad513b15089
Alicja Cichocka <alka.975@gmail.com>
2024-08-26 20:07:59 +0200

    Zadanie Hello World!

---
+++ kodilla-intro/src/main/java/com/kodilla/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.kodilla;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Alicja");
+        System.out.println(27);
+        System.out.println('C');
+    }
+}
