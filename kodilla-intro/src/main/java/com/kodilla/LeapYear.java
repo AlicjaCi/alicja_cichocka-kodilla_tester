5b03cf3c77cabec587a5c1629ffe56ab78e1d816
Alicja Cichocka <alka.975@gmail.com>
2024-08-29 20:50:17 +0200

    Leap Year

--- kodilla-intro/src/main/java/com/kodilla/LeapYear.java
+++ kodilla-intro/src/main/java/com/kodilla/LeapYear.java
@@ -2,10 +2,19 @@ package com.kodilla;
 
 public class LeapYear {
     public static void main(String[] args) {
-        int year = 2020;
+        int year = 2024;
 
-        if (year % 4 == 0) {
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)) {
+            System.out.println(year + " to jest rok przestępny");
 
+        } else {
+            System.out.println(year + " to nie jest rok przestępny");
         }
+
+
+
+
+
+
     }
 }
